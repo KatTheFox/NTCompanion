41aa034f05b01a59a87156f295f9f46062460eaf
KatTheFox <katthefoxmaid@gmail.com>
2022-06-13 21:11:25 -0400

    chore: added debug option

--- src/jsonParse.ts
+++ src/jsonParse.ts
@@ -4,15 +4,19 @@ import { readFileSync } from "fs-extra";
 import * as JSONC from "jsonc-parser";
 import { APIData, SaveData } from "./constants";
 import { getSaveFile } from "./utils";
+const debug = true;
+const overrideKey = "CGHLNRSY8";
+const overrideID = "76561198163343971";
 async function main() {
   const saveFile = getSaveFile();
   const rawJson = readFileSync(saveFile, "utf-8");
   const saveData = JSONC.parse(rawJson) as SaveData;
-  if (saveData.options.streamkey == null) {
-    error(
-      "No stream key found! please enable stream key in Nuclear THrone's settings, then restart this program"
-    );
-  }
+  if (debug)
+    if (saveData.options.streamkey == null) {
+      error(
+        "No stream key found! please enable stream key in Nuclear Throne's settings, then restart this program"
+      );
+    }
   if (saveData.options.streamid == null) {
     error(
       "It looks like you're playing on a non-steam version of Nuclear Throne. The Nuclear Throne API requires you to be playing on the Steam version."
@@ -27,4 +31,4 @@ async function main() {
   const data = response.data as APIData;
   console.log(data);
 }
-console.log(parseInt("00000000000000000000010000000"));
+main();
