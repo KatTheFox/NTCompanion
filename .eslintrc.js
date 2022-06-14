92614fbcd32fc4dbb4decd276427cbb6c4bc01b0
KatTheFox <katthefoxmaid@gmail.com>
2022-06-14 00:06:50 -0400

    chore: oops

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,13 +8,12 @@ module.exports = {
   ],
 
   parserOptions: {
-    // ESLint needs to know about the project's TypeScript settings in order for
-    // TypeScript-specific things to lint correctly. We do not point this at "./tsconfig.json"
-    // because certain files (such at this file) should be linted but not included in the actual
-    // project output.
+    // ESLint needs to know about the project's TypeScript settings in order for TypeScript-specific
+    // things to lint correctly. We do not point this at "./tsconfig.json" because certain files
+    // (such at this file) should be linted but not included in the actual project output.
     project: "./tsconfig.eslint.json",
   },
 
   // We modify the linting rules from the base for some specific things.
-  rules: { "no-bitwise": "allow" },
+  rules: { "no-bitwise": 0 },
 };
